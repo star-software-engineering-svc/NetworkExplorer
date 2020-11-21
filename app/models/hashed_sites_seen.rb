cbdc68c4fbc884bdfbed8bfab1b41088a1a0d183
Tom Racanelli <tomracanelli@outlook.com>
2020-11-20 20:42:07 -0800

    add some models

---
+++ app/models/hashed_sites_seen.rb
@@ -0,0 +1,7 @@
+class HashedSitesSeen
+    include Mongoid::Document
+
+    field :timestamp, type:Integer
+    field :hashed_site, type:String
+    field :"servername", type:String
+end
---
+++ app/models/rawhashes_seen.rb
@@ -0,0 +1,8 @@
+class RawhashesSeen
+    include Mongoid::Document
+
+    field :timestamp, type:Integer
+    field :site_type, type:String
+    field :rawhash, type:String
+    field :"servername", type:String
+end
